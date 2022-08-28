08f8f84bf441c60c6b84e72f666676767fac4c16
1999yfl <35833978+1999yfl@users.noreply.github.com>
2022-08-28 22:26:09 +0800

    clone pydp with py3

---
+++ pydp/tests/test_concentration_sampler.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from math import log
+
+from pydp.samplers.concentration import GammaPriorConcentrationSampler
+
+sampler = GammaPriorConcentrationSampler(0.01, 0.01)
+
+x = 0
+
+for i in range(1000000):
+    x = sampler.sample(x, 1, 100)
+
+    x /= 2
+
+    print(x)
+
+    log(x)
